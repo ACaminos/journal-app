cc85b7538adcf64627cb4e12df45b5071ac7438d
Ariel <caminosariel@gmail.com>
2024-05-22 17:08:14 -0300

    init section 17, styles with MUI

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,13 @@
+import { MailOutline } from "@mui/icons-material"
+import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
+    <JournalLayout>
+      <Typography>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab animi maxime placeat deserunt est corporis sed quisquam, minus doloremque porro libero excepturi? Atque impedit explicabo quaerat quas, expedita dolorum inventore.
+      </Typography>
+    </JournalLayout>
   )
 }
