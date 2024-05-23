196ab2bf27a771db0e8c8b01f672e075de04033b
Ariel <caminosariel@gmail.com>
2024-05-23 18:10:09 -0300

    finish section 17

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,7 @@
-import { Box } from '@mui/material'
+import { Box, Toolbar } from '@mui/material'
 import React from 'react'
+import { NavBar, SideBar } from '../components/';
+
 
 const drawerWidth = 248;
 
@@ -7,11 +9,13 @@ export const JournalLayout = ( { children } ) => {
   return (
     <Box sx={ { display: 'flex' } }>
         {/* Navbar drawerWidth */}
+        <NavBar drawerWidth={ drawerWidth } />
 
         {/* Sidebar drawerWidth*/}
+        <SideBar drawerWidth={ drawerWidth } />
 
         <Box component={'main'} sx={ { flexGrow : 1, p : 3 } }>
-            {/* Toolbar */}
+            <Toolbar/>
 
             { children }
         </Box>
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,21 @@
-import { MailOutline } from "@mui/icons-material"
-import { Typography } from "@mui/material"
+import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView } from "../views/NoteView"
+import { NothingSelectedView } from "../views/NothingSelectedView"
+import { AddOutlined } from "@mui/icons-material"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab animi maxime placeat deserunt est corporis sed quisquam, minus doloremque porro libero excepturi? Atque impedit explicabo quaerat quas, expedita dolorum inventore.
-      </Typography>
+      {/* <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab animi maxime placeat deserunt est corporis sed quisquam, minus doloremque porro libero excepturi? Atque impedit explicabo quaerat quas, expedita dolorum inventore.</Typography> */}
+
+      <NothingSelectedView/>
+
+      {/* <NoteView /> */}
+
+      <IconButton size="large" sx={ { color : 'white', backgroundColor : 'error.main', ':hover': { opacity : 0.9 }, position : 'fixed', right : 50, bottom : 50 } }>
+        <AddOutlined sx={ { fontSize : 30 } }/>
+      </IconButton>
     </JournalLayout>
   )
 }
