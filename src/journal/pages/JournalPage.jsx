5c69a621fa31cc04d032190c7baed26c88c50c22
Ariel Caminos <caminosariel@gmail.com>
2024-12-18 20:42:05 -0300

    notes firebase data

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -3,8 +3,13 @@ import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView } from "../views/NoteView"
 import { NothingSelectedView } from "../views/NothingSelectedView"
 import { AddOutlined } from "@mui/icons-material"
+import { useDispatch } from "react-redux"
+import { startNewNote } from "../../store/journal/thunks"
 
 export const JournalPage = () => {
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => { dispatch( startNewNote() ); }
   return (
     <JournalLayout>
       {/* <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab animi maxime placeat deserunt est corporis sed quisquam, minus doloremque porro libero excepturi? Atque impedit explicabo quaerat quas, expedita dolorum inventore.</Typography> */}
@@ -13,7 +18,7 @@ export const JournalPage = () => {
 
       {/* <NoteView /> */}
 
-      <IconButton size="large" sx={ { color : 'white', backgroundColor : 'error.main', ':hover': { opacity : 0.9 }, position : 'fixed', right : 50, bottom : 50 } }>
+      <IconButton onClick={ onClickNewNote } size="large" sx={ { color : 'white', backgroundColor : 'error.main', ':hover': { opacity : 0.9 }, position : 'fixed', right : 50, bottom : 50 } }>
         <AddOutlined sx={ { fontSize : 30 } }/>
       </IconButton>
     </JournalLayout>
